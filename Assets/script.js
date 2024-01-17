0ab539e3498506f98a4e91e3bb7f579822d92b90
EmilioColds <emiliofriastapia2705@outlook.com>
2024-01-17 16:25:51 -0600

    Added the questions and answers as objects. Also created the first function to start the quiz when the button is clicked.

--- Assets/script.js
+++ Assets/script.js
@@ -22,4 +22,63 @@ document.addEventListener("DOMContentLoaded", () => {
     goBackButton.addEventListener("click", goBack);
     clearHighscoresButton.addEventListener("click", clearHighscores);
 
+    var questions = [
+        {
+            question1: "Commonly used data types DO NOT include: ",
+            answers1: [
+                { text: "Strings", correct: false},
+                { text: "Booleans", correct: false},
+                { text: "Alerts", correct: true},
+                { text: "Numbers", correct: false},
+            ],
+        },
+        {
+            question2: "The condition in an if/else statement is enclosed within ____. ",
+            answers2: [
+                { text: "Quotes", correct: false},
+                { text: "Curly brackets", correct: false},
+                { text: "Parenthesis", correct: true},
+                { text: "Square brackets", correct: false},
+            ],
+        },
+        {
+            question3: "Arrays in JavaScript can be used to store ____. ",
+            answers3: [
+                { text: "Numbers and strings", correct: false},
+                { text: "Other arrays", correct: false},
+                { text: "Booleans", correct: false},
+                { text: "All of the above", correct: true},
+            ],
+        },
+        {
+            question4: "String values must be enclosed within ____ when being assigned to variables. ",
+            answers4: [
+                { text: "Commas", correct: false},
+                { text: "Curly brackets", correct: false},
+                { text: "Quotes", correct: true},
+                { text: "Parenthesis", correct: false},
+            ],
+        },
+        {
+            question5: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+            answers5: [
+                { text: "JavaScript", correct: false},
+                { text: "Terminal / bash", correct: false},
+                { text: "For loops", correct: false},
+                { text: "Console.log", correct: true},
+            ],
+        },
+    ];
+    
+    function startQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        startQuiz.classList.add("hide");
+        timer = 60;
+        clock.innerText = "%{timer} seconds";
+        questionsSection.classList.remove("hide");
+        // startClock(); Declarar funciones para después
+        // setNextQuestion(); Declarar funciones para después
+    }
+
 });
